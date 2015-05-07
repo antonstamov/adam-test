becc82249d99b0e76ff1192e002a3f7998c30d48
biodt <biodt@node01.biodtlab.com>
2015-05-07 15:19:45 +0200

    .

--- build.sbt
+++ build.sbt
@@ -12,6 +12,27 @@ libraryDependencies += "org.scalatest" %% "scalatest" % "2.2.4" % "test"
 
 libraryDependencies += "org.bdgenomics.adam" % "adam-core" % "0.16.0"
 
+com.github.retronym.SbtOneJar.oneJarSettings
 
+mainClass in oneJar := Some("App")
+
+assemblyJarName in assembly := "adam-test.jar"
+
+assemblyOption in assembly := (assemblyOption in assembly).value.copy(includeScala = false)
+
+assemblyMergeStrategy in assembly <<= (assemblyMergeStrategy in assembly) { (old) =>
+{
+  case m if m.toLowerCase.matches("meta-inf/.*\\.sf$") => MergeStrategy.discard
+  case PathList("javax", "servlet", xs @ _*) => MergeStrategy.first
+  case PathList("org", "apache", xs @ _*) => MergeStrategy.first
+  case PathList("com", "esotericsoftware", xs @ _*) => MergeStrategy.last
+  case "about.html" => MergeStrategy.rename
+  case PathList("META-INF", "MANIFEST.MF") => MergeStrategy.discard
+  case x =>  MergeStrategy.last
+}  }
+
+assemblyExcludedJars in assembly <<= (fullClasspath in assembly) map { cp =>
+  cp filter {_.data.getName.contains(".hadoop.")}
+}
 
 fork in run := true
--- src/main/scala/App.scala
+++ src/main/scala/App.scala
@@ -2,30 +2,32 @@ import org.apache.spark.{SparkConf, SparkContext}
 import org.bdgenomics.adam.rdd.ADAMContext
 
 object App  extends App {
+    System.setProperty("HADOOP_USER_NAME", "hdfs")
   val sparkConf = new SparkConf()
-    .setMaster("local[*]")
+    // .setMaster("local[*]")
+    // .setSparkHome("$SPARK_HOME")
     .setAppName("spark-context")
-    .set("spark.driver.host","node01.biodtlab.com")
-    .set("spark.driver.port","35710")
-    .set("spark.yarn.historyServer.address","http://node01.biodtlab.com:18088")
+    // .set("spark.driver.host","node01.biodtlab.com")
+    // .set("spark.driver.port","35710")
+    // .set("spark.yarn.historyServer.address","http://node01.biodtlab.com:18088")
     // .set("spark.driver.port", "41420")
     // .set("spark.serializer", "org.apache.spark.serializer.KryoSerializer")
 
   val sc = new SparkContext(sparkConf)
-  val rdd = sc.textFile("/home/biodt/activator/adam-test/LICENSE")
-    rdd.collect().foreach(println)
+//   val rdd = sc.textFile("/user/biodt/fuu/ME014_dotarea-cu-echipament-a-institutiilor-de-invatamant-secundar_20-09-2010.csv")
+    // rdd.collect().foreach(println)
 
-//   val ac = new ADAMContext(sc)
-//   // Load alignments from disk
-//   val reads = ac.loadAlignments("/home/user/SRR062634_1.filt.fastq")
-//   // Generate, count and sort 21-mers
-//   val kmers = reads.flatMap { read =>
-//     read.getSequence.sliding(21)//.map(k => (k, 1L))
-//   }.groupBy(x=>x).map(x=> (x._1,x._2.size))//.reduceByKey((k1: Long, k2: Long) => k1 + k2)
-//     /*.map(_.swap)
-//     .sortBy(_._1)*///ByKey(ascending = false)
-//   // Print the top 10 most common 21-mers
-//   kmers.take(10).foreach(println)
+  val ac = new ADAMContext(sc)
+  // Load alignments from disk
+  val reads = ac.loadAlignments("/user/biodtfs/a7cf092f-1543-4d9d-af42-3f07c8f3af6a.bam")
+  // Generate, count and sort 21-mers
+  val kmers = reads.flatMap { read =>
+    read.getSequence.sliding(21)//.map(k => (k, 1L))
+  }.groupBy(x=>x).map(x=> (x._1,x._2.size))//.reduceByKey((k1: Long, k2: Long) => k1 + k2)
+    /*.map(_.swap)
+    .sortBy(_._1)*///ByKey(ascending = false)
+  // Print the top 10 most common 21-mers
+  kmers.take(10).foreach(println)
 
 
 /* Array(
